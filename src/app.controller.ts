31d9e6e1ccd0d06a97aa515a37e43d9dbfd79438
krishbale <sushil.pokharel.7528@gmail.com>
2023-07-02 11:22:23 +0545

    avatar url added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -22,7 +22,7 @@ export class AppController {
         'https://discord.com/api/webhooks/1124927168984645802/E8ZIt-OL39wSweqjiR2aJKCUNPKoRRXlKwmRw5aI2dFa6lyxQ1SlM8SGiipZSecvWpjd',
         {
           content: ':tada: New Order',
-          avaratraurl: body.repository.url,
+          avaratraurl: body.sender.avatar_url,
         },
       )
       .subscribe({
