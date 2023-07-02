803ec33a8d14895801566a98359f5ebd918d6b08
krishbale <sushil.pokharel.7528@gmail.com>
2023-07-02 11:36:40 +0545

    modifying url

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,11 +17,12 @@ export class AppController {
   createorder(@Body() body: any) {
     console.log(body.repository.name);
     const avatarurl = body.sender.avatar_url;
-    const content = `New push from ${body.repository.name} <hr>
+    const content = `Hello sir, there is a new commit on 
+   Repository: ${body.repository.name} <hr>
      with commit message ${body.head_commit.message}
-      <hr> by ${body.head_commit.author.name} <hr>
-      ${body.head_commit.url}<hr>
-     <hr>  ${body.head_commit.message}`;
+      <hr> by Auther: ${body.head_commit.author.name} <hr>
+      View commit url: ${body.head_commit.url}<hr>
+     <hr> View commit message: ${body.head_commit.message}`;
     this.httpService
       .post(
         'https://discord.com/api/webhooks/1124927168984645802/E8ZIt-OL39wSweqjiR2aJKCUNPKoRRXlKwmRw5aI2dFa6lyxQ1SlM8SGiipZSecvWpjd',
